27aeaf6b6ce1a6314b552c259e04dc2d99f454b0
Martin Musil <martinmusildev@gmail.com>
2024-12-31 12:33:53 +0100

    V1.1.0
    - Added confirmation message before proceeding with the update message

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import constants
+import asyncio
 from discord.ext import commands
 
 from utils import send_avalore_update_message, read_avalore_update_file
@@ -20,6 +21,17 @@ async def announce_avalore_update(ctx: commands.Context) -> None:
     if ctx.author.id != constants.MUSKADEV_ID: # Check if the user invoking the command is muskadev
         return # Ignore the command if the user is not muskadev
     
-    channel = Client.get_channel(constants.CHANNEL_ID_PATCHES) # Get the 'channel' object from the channel ID
-    
-    await send_avalore_update_message(channel, read_avalore_update_file('avalore_update_data.txt')) # Send the update message to the 'patches' channel
+    # Ask for confirmation before sending the update message to the 'patches' channel
+    await ctx.send("Are you sure you filled fresh update data? Reply with 'yes' to confirm.")
+
+    # Function to check if the message author is the same as the command invoker and the message content is 'yes'
+    def check(m):
+        return m.author == ctx.author and m.content.lower() == 'yes'
+
+    try: # Try to wait for a message that satisfies the check function with a timeout of 'AVALORE_UPDATE_TIMOUT_LIMIT' seconds
+        confirmation = await Client.wait_for('message', check=check, timeout=constants.AVALORE_UPDATE_TIMOUT_LIMIT)
+        if confirmation: # If the confirmation message is received
+            channel = Client.get_channel(constants.CHANNEL_ID_PATCHES) # Get the 'channel' object from the channel ID
+            await send_avalore_update_message(channel, read_avalore_update_file('avalore_update_data.txt')) # Send the update message to the 'patches' channel
+    except asyncio.TimeoutError: # If the timeout limit is reached without receiving the confirmation message (the user didn't reply with 'yes')
+        await ctx.send("Update announcement cancelled. Please try again.") # Send a message to the command invoker that the update announcement is cancelled
